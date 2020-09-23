2bbaf9736a44044558e4f95a33cb323f7ee90ff5
bayliebevers <71790358+bayliebevers@users.noreply.github.com>
2020-09-23 16:09:27 -0500

    Update sql.test.2.sql

--- sql.test.2.sql
+++ sql.test.2.sql
@@ -4,4 +4,4 @@ CREATE TABLE Products (
 product_name varchar(225),
 number_of_purchases DOUBLE);
 
-INSERT INTO Clients (product_name, number_of_purchases) VALUES (apple,3) (laptop,2) (jetpack,1);
+INSERT INTO Products (product_name, number_of_purchases) VALUES (apple,3) (laptop,2) (jetpack,1);

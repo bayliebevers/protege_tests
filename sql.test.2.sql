2aab1b40b89c08099f318421e22312bb80079dcb
bayliebevers <71790358+bayliebevers@users.noreply.github.com>
2020-09-23 16:05:15 -0500

    Update sql.test.2.sql

--- sql.test.2.sql
+++ sql.test.2.sql
@@ -1,1 +1,7 @@
 -- Create a Sql query in MSSql or MySql with 2 columns: product_name, number_of_purchases
+
+CREATE TABLE Products (
+product_name varchar(225),
+number_of_purchases DOUBLE);
+
+INSERT INTO Clients (product_name, number_of_purchases) VALUES (apple,3) (laptop,2) (jetpack,1);

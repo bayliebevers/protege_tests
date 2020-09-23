8b0c2210ae322087ba61990cd56adb5152107ca7
bayliebevers <71790358+bayliebevers@users.noreply.github.com>
2020-09-23 16:08:41 -0500

    Update sql.test.3.sql

--- sql.test.3.sql
+++ sql.test.3.sql
@@ -1,1 +1,7 @@
 -- Create a Sql query in MSSql or MySql with 2 columns: client_name, sum_of_purchases
+
+CREATE TABLE SumOfPurchases (
+client_name varchar(225),
+sum_of_purchases DOUBLE);
+
+INSERT INTO SumOfPurchases (client_name, number_of_purchases) VALUES (Fran,25901) (David,1) (Adrian,901);

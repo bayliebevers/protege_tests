1c0f2940cc97e9d8b328cc92889ed930b0675462
bayliebevers <71790358+bayliebevers@users.noreply.github.com>
2020-09-24 13:14:43 -0500

    Update sql.test.1.sql
    Corrected Test 1

--- sql.test.1.sql
+++ sql.test.1.sql
@@ -1,7 +1,6 @@
 -- Create a Sql query in MSSql or MySql with 2 columns: client_name, number_of_purchases
 
-CREATE TABLE Clients (
-client_name varchar(225),
-number_of_purchases DOUBLE);
+SELECT customers.name, SUM(purchases.product_id)
+FROM customers, purchases
+JOIN customers ON customers.id=purchases.customer_id 
 
-INSERT INTO Clients (client_name, number_of_purchases) VALUES (Fran,3) (David,1) (Adrian,2);
